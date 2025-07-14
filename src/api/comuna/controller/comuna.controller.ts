95f8e1b3fc260955ed83b075e1e42f8682dcfc15
Jose Hernndez <jhernandez@kiosolutions.com>
2025-07-14 01:31:15 -0400

    fix comunas

--- src/api/comuna/controller/comuna.controller.ts
+++ src/api/comuna/controller/comuna.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Get, ParseIntPipe, Param } from "@nestjs/common";
+import { Controller, Post, Body, Get, ParseIntPipe, Param, Patch } from "@nestjs/common";
 import { ComunaService } from "../service/comuna.service";
 import { FilterExpenseDto } from "../dto/FilterComuna.dto";
 import { GenericResponse } from "src/api/genericDto/GenericResponse";
+import { ComunaDto } from "../dto/comuna.dto";
 
 @Controller('/comunas')
 export class ComunaController {
@@ -9,13 +10,31 @@ export class ComunaController {
     constructor(private readonly comunaService: ComunaService) { }
 
     //@UseGuards(JwtAuthGuard)
+    @Get("/")
+    async getAll(): Promise<GenericResponse> {
+        return await this.comunaService.getAll();
+    }
+
+    //@UseGuards(JwtAuthGuard)
+    @Post("/")
+    async new(@Body() comuna: ComunaDto): Promise<GenericResponse> {
+        return await this.comunaService.new(comuna);
+    }
+
+    //@UseGuards(JwtAuthGuard)
+    @Patch("/:codigo")
+    async edit(@Body() comuna: ComunaDto, @Param("codigo", ParseIntPipe) codigo: number): Promise<GenericResponse> {
+        return await this.comunaService.edit(comuna, codigo);
+    }
+
+    //@UseGuards(JwtAuthGuard)
     @Get("/:codigo")
-    async getExpenses(@Param("codigo", ParseIntPipe) codigo: number): Promise<GenericResponse> {
+    async delete(@Param("codigo", ParseIntPipe) codigo: number): Promise<GenericResponse> {
         return await this.comunaService.delete(codigo);
     }
 
     @Post('/findByFilter')
-    getAll(@Body() filterExpenseDto: FilterExpenseDto): Promise<GenericResponse> {
+    findByFilter(@Body() filterExpenseDto: FilterExpenseDto): Promise<GenericResponse> {
         return this.comunaService.findByFilter(filterExpenseDto);
     }
 }
--- src/api/comuna/service/comuna.service.ts
+++ src/api/comuna/service/comuna.service.ts
@@ -4,6 +4,7 @@ import { Like, Repository } from 'typeorm';
 import { Comunas } from '../entities/comuna.entity';
 import { FilterExpenseDto } from '../dto/FilterComuna.dto';
 import { GenericResponse, StatusCode } from 'src/api/genericDto/GenericResponse';
+import { ComunaDto } from '../dto/comuna.dto';
 
 @Injectable()
 export class ComunaService {
@@ -12,21 +13,105 @@ export class ComunaService {
     private comunaRepository: Repository<Comunas>,
   ) { }
 
-  async getAll(codigo: number): Promise<GenericResponse> {
+  async getAll(): Promise<GenericResponse> {
     // console.log('method getAll');
     let resp: GenericResponse = new GenericResponse();
+    let dataResponse: Comunas[] = [];
+    try {
+      dataResponse = await this.comunaRepository.find({
+        select: ['codigo', 'descrip', 'estado']
+      });
+    } catch (error) {
+      resp.code = '-1';
+      resp.message = StatusCode.ERROR + ': ' + error;
+      resp.data = null;
+      console.log(JSON.stringify(resp));
+      return resp;
+    }
+    if (dataResponse.length === 0) {
+      resp.code = '-2';
+      resp.message = StatusCode.ERROR + ', Sin Registros';
+      resp.data = null;
+      return resp;
+    }
+    resp.data = this.convertToVOs(dataResponse);
     return resp;
   }
 
-  async new(codigo: number): Promise<GenericResponse> {
+  async new(comuna: ComunaDto): Promise<GenericResponse> {
     // console.log('method new');
     let resp: GenericResponse = new GenericResponse();
+    let dataResponse: Comunas = new Comunas();
+    try {
+      const { codigo, descrip } = comuna;
+      try {
+        let toNew: Comunas = await this.comunaRepository.findOneBy([{ codigo }, { descrip }]);
+        if (toNew) {
+          resp.code = '-4';
+          resp.data = null;
+          resp.message = 'Comuna ya existe';
+          return resp;
+        }
+      } catch (error) {
+        // console.log(JSON.stringify(error));
+        resp.code = '-3';
+        resp.message = StatusCode.ERROR;
+        resp.data = null;
+        return resp;
+      }
+
+      try {
+        const newElement = Object.assign(new Comunas(), {
+          codigo, descrip, estado: true
+        });
+        dataResponse = await this.comunaRepository.save(newElement);
+        resp.data = dataResponse.codigo;
+      } catch (error) {
+        // console.log(JSON.stringify(error));
+        resp.code = '-2';
+        resp.message = StatusCode.ERROR;
+        resp.data = null;
+      }
+    } catch (error) {
+      // console.log(JSON.stringify(error));
+      resp.code = '-1';
+      resp.message = StatusCode.ERROR;
+      resp.data = null;
+    }
     return resp;
   }
 
-  async edit(codigo: number): Promise<GenericResponse> {
+  async edit(comuna: ComunaDto, codigo: number): Promise<GenericResponse> {
     // console.log('method edit');
     let resp: GenericResponse = new GenericResponse();
+    let dataResponse: Comunas = new Comunas();
+    let elementToEdit: Comunas = new Comunas();
+    try {
+      //const codigo = codigo);
+      elementToEdit = await this.comunaRepository.findOneBy({ codigo });
+      if (!elementToEdit) {
+        resp.code = '-3';
+        resp.data = new Comunas();
+        console.log('Comuna no existe');
+        return resp;
+      }
+    } catch (error) {
+      console.log(JSON.stringify(error));
+      resp.code = '-2';
+      resp.message = StatusCode.ERROR;
+      resp.data = null;
+      return resp;
+    }
+
+    try {
+      elementToEdit = this.getObjectEdit(comuna, elementToEdit);
+      dataResponse = await this.comunaRepository.save(elementToEdit);
+    } catch (error) {
+      console.log(JSON.stringify(error));
+      resp.code = '-1';
+      resp.message = StatusCode.ERROR;
+      resp.data = null;
+    }
     return resp;
   }
 
@@ -90,6 +175,37 @@ export class ComunaService {
       resp.data = null;
     }
     return resp;
+  }
+
+  // Utility Method.
+  private convertToVOs(inputUser: Comunas[]): ComunaDto[] {
+    let salidaUser: ComunaDto[] = [];
+    for (let index = 0; index < inputUser.length; index++) {
+      salidaUser.push(this.convertToVO(inputUser[index]));
+    }
+    return salidaUser;
+  }
+
+  private convertToVO(inputUser: Comunas): ComunaDto {
+    let itemUser: ComunaDto = new ComunaDto();
+    itemUser = new ComunaDto();
+    itemUser.codigo = inputUser.codigo;
+    itemUser.descrip = inputUser.descrip;
+    itemUser.estado = inputUser.estado;
+    return itemUser;
+  }
+
+  private getObjectEdit(request: ComunaDto, elementToEdit: Comunas): Comunas {
+    const { descrip, estado } = request;
+    if (typeof descrip !== 'undefined' && descrip !== null && descrip !== '') {
+      console.log('descrip: [' + descrip + ']');
+      elementToEdit.descrip = descrip;
+    }
+    if (typeof estado !== 'undefined' && estado !== null) {
+      console.log('estado: [' + estado + ']');
+      elementToEdit.estado = estado;
+    }
+    return elementToEdit;
   }
 
 }
